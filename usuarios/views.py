50fd8cf7ca2795b386489f6880ccc598401f5450
Gabriel Cadête <gabriel4662@hotmail.com>
2025-04-06 23:46:37 -0300

    commit input data

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def cadastro(request):
     if request.method == 'GET':    
         return render(request, 'cadastro.html')
+   
     elif request.method == 'POST':
         username = request.POST.get('username')
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
-        return HttpResponse(f'Nome do Usuário: {username} - Senha: {senha} - Confirmar Senha: {confirmar_senha}')
+
+        if senha != confirmar_senha:
+            return redirect ('/usuarios/cadastro/')
+
+        if len(senha) < 6:
+            return redirect ('/usuarios/cadastro/')
+        
+        users = User.objects.filter(username=username) 
+        if users.exists():
+            return redirect ('/usuarios/cadastro/')
+
+
+
+        User.objects.create_user(
+            username=username,
+            password=senha,
+        )
+
+        return redirect ('/usuarios/login/')
